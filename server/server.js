c114251f564a25a0fd570bc993290064b72a35a7
devinMcArthur <assortedgerm@gmail.com>
2019-01-14 10:53:51 -0400

    Heroku prep 3.3 - add console log

--- server/server.js
+++ server/server.js
@@ -170,6 +170,7 @@ app.use((req, res, next) => {
 
     return fetchComponentData(store, renderProps.components, renderProps.params)
       .then(() => {
+        console.log("FETCHCOMPONENTDATA");
         const initialView = renderToString(
           <Provider store={store}>
             <IntlWrapper>
